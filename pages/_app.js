9095c0c22e5f7e51bec36a644e234b48cdb34df4
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2022-10-02 20:56:18 -0500

    pendejadas, totalmente pendejadas

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,57 @@
-import '../styles/globals.css'
+import { GetServerSidePropsContext } from "next";
+import { useState } from "react";
+import { AppProps } from "next/app";
+import { getCookie, setCookie } from "cookies-next";
+import Head from "next/head";
+import {
+  MantineProvider,
+  ColorScheme,
+  ColorSchemeProvider,
+} from "@mantine/core";
+import { NotificationsProvider } from "@mantine/notifications";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App(props) {
+  const { Component, pageProps } = props;
+  const [colorScheme, setColorScheme] = useState(props.colorScheme);
+
+  const toggleColorScheme = (value) => {
+    const nextColorScheme =
+      value || (colorScheme === "dark" ? "light" : "dark");
+    setColorScheme(nextColorScheme);
+    setCookie("mantine-color-scheme", nextColorScheme, {
+      maxAge: 60 * 60 * 24 * 30,
+    });
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Mantine next example</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
+
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          theme={{ colorScheme }}
+          withGlobalStyles
+          withNormalizeCSS
+        >
+          <NotificationsProvider>
+            <Component {...pageProps} />
+          </NotificationsProvider>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+App.getInitialProps = ({ ctx }) => ({
+  colorScheme: getCookie("mantine-color-scheme", ctx) || "light",
+});
